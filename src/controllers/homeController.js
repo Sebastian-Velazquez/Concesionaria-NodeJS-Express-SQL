dde037d9ecddd700b900c2187cba098c97361902
Sebastian-Velazquez <sergiovelazquez1693@gmail.com>
2023-01-16 11:54:03 -0300

    pagina index dinamica listo

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,16 +1,28 @@
+const fs = require('fs');
+const path = require('path'); 
+
+/* En la constante "products" ya tienen los productos que están 
+guardados en la carpeta Data como Json (un array de objetos literales) */
+const productsFilePath = path.join(__dirname, '../data/productsDataBase.json');
+/* const products = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8')); */
+
 const controlador ={ //IMPORTANTE
     index: (req, res)=>{ //renderisar o mustrar una vista(pagina como antes)
-        return res.render('index'); //no es necesario la estencion, lo hago para recordar. es para mostrar un archivo en la pagina
+        const products = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
+        /* console.log(products) */
+        let productosVisitados = products.filter( producto => { //filtro en el main
+            return producto.category == "visited"
+        });
+
+        let productosEnOferta = products.filter( producto => { //filtro en el main
+            return producto.category == "in-sale"
+
+        });
+        res.render("index",{
+            productosVisitados: productosVisitados, //No es necesario el mismo nombre.. Para ejs se usa la izquierda
+            productosEnOferta: productosEnOferta}); 
     }
-    //para poner datos en una pagina
-    /* oferta:(req, res)=>{
-            let listaOferta = [
-                'lavadora',
-                'licuadora',
-                'destornillador',
-            ];                      //listaOferta de la izquierda es el que va en la pagina
-            res.render('ofertas',{'listaOferta':listaOferta})
-    }, */
 }
+
         //exportamos el objeto literal con sus metodos
         module.exports = controlador;
