507a72bac3e7ca95f787d02c72a4888829feefbc
Sebastian-Velazquez <sergiovelazquez1693@gmail.com>
2023-01-16 15:11:56 -0300

    vista de editar producto completo

--- src/controllers/productsControllers.js
+++ src/controllers/productsControllers.js
@@ -11,7 +11,7 @@ const controlador ={ //IMPORTANTE
         return res.render('./products/productCart');
     },
 
-    //-----------------------------DETALLE DE PRODUCTO---------------------------------------
+    //*****************DETALLE DE PRODUCTO************
     //Ruta: /products/detail
     productDetail:(req, res)=>{
         let id = req.params.id //esto es lo que nos llega por parametro
@@ -23,31 +23,34 @@ const controlador ={ //IMPORTANTE
         })
         res.render('./products/productDetail',{producto:productFiltrado});
     },
-    //-------------------------LISTA DE TODOS LOS PRODUCTOS------------
+
+
+    //*****************LISTA DE TODOS LOS PRODUCTOS************
     //Ruta: /products/list
     list:(req, res)=>{
         const products = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
         res.render('./products/listProduct',{'listaProductos':products})//es 'prodct' porque acordate que es el archivo .ejs el que.. antes coincidia.. se cambio para ser mas claros.
     },
-
+    //*****************CREAR PRDUCTO************
     creationPrduct:(req, res)=>{
         return res.render('./products/creationPrduct');
     },
-    editProduct:(req, res)=>{
-        return res.render('./products/editProduct');
-    },
-    
-    //para poner datos en una pagina
-    /* oferta:(req, res)=>{
-            let listaOferta = [
-                'lavadora',
-                'licuadora',
-                'destornillador',
 
-            ];
 
-            res.render('ofertas',{'listaOferta':listaOferta})
-    }, */
+    //*****************EDITAR PRODUCTO************
+    //ruta: /products/edit
+    edit:(req, res)=>{
+        let id = req.params.id //esto es lo que nos llega por parametro
+        
+        const products = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
+        //Retorna el producto con el id mandado del req.params.id
+        let productFiltrado = products.find(producto=>{
+            return producto.id == id;
+        })
+
+        //Listo para mandar a .ejs//se pone el nombre del ejs entre ''.
+        res.render('./products/productEdit',{producto:productFiltrado,});
+    },   
 }
 
         //exportamos el objeto literal con sus metodos
--- src/routes/productsRouter.js
+++ src/routes/productsRouter.js
@@ -7,8 +7,6 @@ const multer = require("multer")
 // llamamos a la ruta de controlador
 const productsControllers = require("../controllers/productsControllers.js")
 
-// procesa pedido de get. Ahora usamos router en MVC. son tutas 
-/* router.get("/list", mainController.list); */ //EJEMPLO DE PRAMETRO COMPARTIDO
 
 //devolver o mandar un producto a detalle de producto
 router.get("/detail/:id", productsControllers.productDetail);
@@ -16,15 +14,19 @@ router.get("/detail/:id", productsControllers.productDetail);
 //Lista de todos los productos
 router.get("/list", productsControllers.list);
 
+//********************GRUD*********************/
+//CREAR
+//EDITAR
+router.get("/edit/:id/", productsControllers.edit);
+/* router.put("/edit/:id/", productsControllers.processEdit); */
+//EMILINAR
+
 
 // procesa pedido de get. Ahora usamos router en MVC. son tutas 
 router.get("/productCart", productsControllers.productCart);
-
-
-
 //router.get("/register", homeController.register);
 router.get("/creationPrduct", productsControllers.creationPrduct);
-router.get("/editProduct", productsControllers.editProduct);
+/* router.get("/editProduct", productsControllers.editProduct); */
 //router.get("/listProduct", productsControllers.listProduct);
 
 /* router.get("/ofertas", homeController.oferta); */
