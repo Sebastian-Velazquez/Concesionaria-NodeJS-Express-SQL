dfeb7c497b1936896b6bb21896b3ac5145c484fe
Sebastian-Velazquez <sergiovelazquez1693@gmail.com>
2023-01-16 21:44:35 -0300

    listo subir y cambiar imagen en editar producto

--- src/routes/productsRouter.js
+++ src/routes/productsRouter.js
@@ -22,25 +22,21 @@ const productsControllers = require("../controllers/productsControllers.js")
 
 //devolver o mandar un producto a detalle de producto
 router.get("/detail/:id", productsControllers.productDetail);
-
 //Lista de todos los productos
 router.get("/list", productsControllers.list);
-
 //********************GRUD*********************/
 //CREAR
 router.get("/create", productsControllers.create);
 router.post("/create", upload.single("productImage"), productsControllers.processCreate);
 //EDITAR
 router.get("/edit/:id/", productsControllers.edit);
-router.put("/edit/:id/", productsControllers.processEdit);
+router.put("/edit/:id/", upload.single("productImage"), productsControllers.processEdit);
 //EMILINAR
 router.delete("/delete/:id/", productsControllers.delete);
-
+//********************GRUD*********************/
 
 
 router.get("/productCart", productsControllers.productCart);
-
-
 /* router.get("/ofertas", homeController.oferta); */
 
 module.exports = router;
