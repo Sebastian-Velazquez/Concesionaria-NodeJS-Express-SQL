b64a4e4534d237a5a84209ce83be4a1a78831cd4
Sebastian-Velazquez <sergiovelazquez1693@gmail.com>
2023-01-29 20:00:29 -0300

    creacion de middlewares

--- src/routes/productsRouter.js
+++ src/routes/productsRouter.js
@@ -1,21 +1,12 @@
 //va siempre
 const express = require("express");
 const router = express.Router();
-const multer = require("multer")
-const path = require("path");//para multer
+//const multer = require("multer")
+//const path = require("path");//para multer
 const { productCart } = require("../controllers/productsControllers.js");
 
-//***********Multer****************
-const storage = multer.diskStorage({
-    destination : function(req, file, cb){
-        cb(null,"public/img/products") //se pone la ruta como si estariamos en la raiz del proyecto.
-    },
-    filename : function(req, file, cb){
-        cb(null, file.fieldname + "-" + Date.now() + path.extname(file.originalname))
-    }
-})
-const upload = multer({storage:storage})
-//***********FinMulter****************
+//Middleware
+const upload = require("../middlewares/multerMiddleware");
 
 // llamamos a la ruta de controlador
 const productsControllers = require("../controllers/productsControllers.js")
