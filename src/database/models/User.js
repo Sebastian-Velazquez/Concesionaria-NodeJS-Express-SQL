9e357b68d0c3b25267779454673d0ea446e2586a
EddieArg88 <tomasmanco@outlook.com>
2023-02-18 19:41:55 -0300

    terminado el model de user

--- src/database/models/User.js
+++ src/database/models/User.js
@@ -1,3 +1,5 @@
+const { FOREIGNKEYS } = require("sequelize/types/query-types");
+
 module.exports = (sequelize, DataTypes) =>{
     let alias = "Usuarios";
     let cols ={
@@ -8,7 +10,27 @@ module.exports = (sequelize, DataTypes) =>{
         },
         first_name:{
             type: DataTypes.STRING
+        },
+        last_name:{
+            type: DataTypes.STRING
+        },
+        birth_date:{
+            type: DataTypes.DATE
+        },
+        email: {
+            type: DataTypes.STRING               
+        },
+        password:{
+            type: DataTypes.STRING
+        },
+        id_category:{
+            type: DataTypes.INTEGER,
+            foreingKey: true,
+        },
+        image:{
+            type: DataTypes.STRING
         }
+
     };
     let config = {
         tableName: "usuarios",//esto se pone porque lo general el nombbre de la tabla el mismo nombre que el js
