8a2f727499ba8ae85f1eac18aae33c3a1dca2e47
EddieArg88 <tomasmanco@outlook.com>
2023-02-18 20:19:26 -0300

    terminado modelo categorias

---
+++ src/database/models/Categorias.js
@@ -0,0 +1,26 @@
+module.exports = (sequelize, dataTypes) => {
+    
+    let alias = "categorias";
+
+    let cols = {
+        id_category: {
+            type: dataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        category_name: {
+            type: {
+                type: dataTypes.STRING
+            }
+        }
+    }
+
+    let config = {
+        tableName : "categorias",
+        timestamps: false
+    }
+
+    const Categoria = sequelize.define(alias, cols, config)
+    return Categoria
+
+}
