5a9b2902b575ec7b21b8ce2d65f8499ae4e0f846
Sebastian-Velazquez <sergiovelazquez1693@gmail.com>
2023-02-26 20:45:51 -0300

    adelantando registro y login de usario, falta controlador

--- src/controllers/usersControllersSql.js
+++ src/controllers/usersControllersSql.js
@@ -1,8 +1,11 @@
 let db = require("../database/models");
 
 const controlador ={
+    register:(req, res)=>{
+        res.send("Estas en el registro de usario")
+    },
     login:(req, res)=>{
-        
+        res.send("Estas en el login de registro")
     }
 }
 module.exports = controlador;
--- src/routes/userRouterSql.js
+++ src/routes/userRouterSql.js
@@ -4,7 +4,8 @@ const router = express.Router();
 // llamamos a la ruta de controlador
 const usersControllersSql = require("../controllers/usersControllersSql.js")
 
-//lista de productos
+//regitro de usuario
+router.get("/register", usersControllersSql.register);
+//login de usuario
 router.get("/login", usersControllersSql.login);
-
 module.exports = router;
