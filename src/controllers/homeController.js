58cedfa04617994067d64bba104509a5ba2d562d
Sebastian-Velazquez <sergiovelazquez1693@gmail.com>
2023-03-03 12:21:05 -0300

    index terminado, pasado a db sql

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -5,12 +5,25 @@ const path = require('path');
 guardados en la carpeta Data como Json (un array de objetos literales) */
 const productsFilePath = path.join(__dirname, '../data/productsDataBase.json');
 /* const products = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8')); */
+let db = require("../database/models");
 
 const controlador ={ //IMPORTANTE
     //-----------------IDEX DINAMICO-----------------------
-    index: (req, res)=>{ //renderisar o mustrar una vista(pagina como antes)
-        const products = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
-        /* console.log(products) */
+    index: (req, res)=>{ 
+        let pedidoProductos = db.Productos.findAll()
+        let pedidoModelos = db.Modelos.findAll()
+        Promise.all([pedidoProductos, pedidoModelos])//para poder llamar dos tablas
+            .then(function([productos, modelos]){
+                res.render("index",{
+                    modelos:modelos,
+                    productos:productos})
+            })
+            .catch(function(error){
+                res.send(error)
+        }) 
+        /**************** */
+
+  /*       const products = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
         let productosSedan = products.filter( producto => { //filtro en el main
             return producto.category == "sedan"
         });
@@ -21,8 +34,8 @@ const controlador ={ //IMPORTANTE
         });
         res.render("index",{
             productosSedan: productosSedan, //No es necesario el mismo nombre.. Para ejs se usa la izquierda
-            productosCoupe: productosCoupe}); 
-    }
+            productosCoupe: productosCoupe}); */
+    } 
 }
 
         //exportamos el objeto literal con sus metodos
