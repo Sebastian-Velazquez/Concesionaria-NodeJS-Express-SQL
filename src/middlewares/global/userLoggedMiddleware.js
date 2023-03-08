1af94811f6f8e1c2e9a6fa5b7923ac3d4fa5829c
Sebastian-Velazquez <sergiovelazquez1693@gmail.com>
2023-03-08 20:53:28 -0300

    haciendo la session cookie

--- src/middlewares/global/userLoggedMiddleware.js
+++ src/middlewares/global/userLoggedMiddleware.js
@@ -4,18 +4,32 @@
 
 
 const User = require("../../models/userModels")
+let db = require("../../database/models");
 
 function userLoggedMiddleware(req, res, next) {
     res.locals.isLogged = false;//res.locals en es variable local.. se puede compartir en todas las vistas
 
+
+    if (req.cookies.user){
+        db.Usuarios.findOne({ //dindOne: busca y hay un dato que sea igual al madado por el body
+            where:{
+                email: req.body.email  //
+            }
+        }).then(dato =>{
+            if (dato){
+                console.log(dato)
+                return req.session.userLogged = userFromCookie;
+            }
+        })
+    }
     /***********COOKIE */
     //Para loguear automaticamente a un usuario si esta en cookie -- se usasa cookie-parser
-    let emailInCookie = req.cookies.userEmail;  //para guardar el usario de la cookie si esta
+  /*   let emailInCookie = req.cookies.userEmail;  //para guardar el usario de la cookie si esta
     let userFromCookie = User.findByField('email', emailInCookie);
     //console.log(userFromCookie);
     if (userFromCookie){//si hay usuario - guarda es session para cuando session pregunte si hay alguien.. ya cookie guardo uno
         req.session.userLogged = userFromCookie;
-    }  
+    }   */
 /***********COOKIE */
 
     //para trabajar en las vistas que el con los datos para rederizar las vistas como por ejemplo, no mostrar una parte si el usuario esta logueado
