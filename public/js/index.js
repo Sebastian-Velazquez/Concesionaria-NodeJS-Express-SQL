b2a4011ae3eb6fc3f0c8e4fed26614472e353450
EddieArg88 <tomasmanco@outlook.com>
2023-03-12 23:49:47 -0300

    validacion front terminada

---
+++ public/js/index.js
@@ -0,0 +1,149 @@
+window.addEventListener("load", () => {
+
+    let form = document.querySelector(".form");
+    form.name.focus();
+    let errorsHtml = document.querySelector(".errores");
+
+    form.addEventListener("submit", (event) => {
+
+        let errors = [];
+
+        /* NAME */
+        if (form.name.value == "") {
+            errors.push("El nombre está vacio");
+            form.name.classList.remove("is-valid")
+            form.name.classList.add("is-invalid")
+        } else if (form.name.value.length < 4 ) {
+            errors.push("El nombre debe tener al menos 4 caracteres");
+            form.name.classList.remove("is-valid")
+            form.name.classList.add("is-invalid")
+        } else {
+            form.name.classList.remove("is-invalid")
+            form.name.classList.add("is-valid")
+        }
+        /* NAME */
+        if (form.lastName.value == "") {
+            errors.push("El apellido está vacio");
+            form.lastName.classList.remove("is-valid")
+            form.lastName.classList.add("is-invalid")
+        } else if (form.lastName.value.length < 4 ) {
+            errors.push("El apellido debe tener al menos 4 caracteres");
+            form.lastName.classList.remove("is-valid")
+            form.lastName.classList.add("is-invalid")
+        } else {
+            form.lastName.classList.remove("is-invalid")
+            form.lastName.classList.add("is-valid")
+        }
+
+        /* CONTRASEÑA */
+        if (form.password.value == "") {
+            errors.push("La contraseña está vacia");
+            form.password.classList.remove("is-valid")
+            form.password.classList.add("is-invalid")
+        } else {
+            form.password.classList.remove("is-invalid")
+            form.password.classList.add("is-valid")
+        }
+        
+        let regEmail = /\S+@\S+\.\S+/;
+        if (!regEmail.test(form.email.value)) {
+            errors.push("Debe ingresar un email válido");
+            email.classList.add("is-invalid");
+            email.classList.remove("is-valid");
+        } else {
+            email.classList.add("is-valid");
+            email.classList.remove("is-invalid");
+            form.password.focus();
+        };
+
+        /* console.log(errors); */
+        if (errors.length > 0) {
+
+            event.preventDefault();
+            errorsHtml.innerHTML = "";
+            errors.forEach(error => {
+                errorsHtml.innerHTML += "<li>" + error + "</li>"
+            });
+        } else {
+            errorsHtml.innerHTML = "";
+            form.submit();
+        }
+    })
+})
+/* window.addEventListener("load", () => {
+
+    let form = document.querySelector(".form");
+    form.name.focus();
+    
+    let errorsHtml = document.querySelector(".errores");
+
+    form.addEventListener("submit", (event) => {
+
+        let errors = []
+
+                
+                if (form.name.value == "") {
+                    errors.push("El nombre está vacio");
+                    form.name.classList.remove("is-valid")
+                    form.name.classList.add("is-invalid")
+                } else if (form.name.value.length < 4 ) {
+                    errors.push("El nombre debe tener al menos 4 caracteres");
+                    form.name.classList.remove("is-valid")
+                    form.name.classList.add("is-invalid")
+                } else {
+                    form.name.classList.remove("is-invalid")
+                    form.name.classList.add("is-valid")
+                }
+
+
+                if (form.lastName.value == "") {
+                    errors.push("El apellido está vacio");
+                    form.lastName.classList.remove("is-valid")
+                    form.lastName.classList.add("is-invalid")
+                } else if (form.lastName.value.length < 4 ) {
+                    errors.push("El apellido debe tener al menos 4 caracteres");
+                    form.lastName.classList.remove("is-valid")
+                    form.lastName.classList.add("is-invalid")
+                } else {
+                    form.lastName.classList.remove("is-invalid")
+                    form.lastName.classList.add("is-valid")
+                }      
+   
+                if (form.password.value == "") {
+                    errors.push("La contraseña está vacia");
+                    form.password.classList.remove("is-valid")
+                    form.password.classList.add("is-invalid")
+                } else {
+                    form.password.classList.remove("is-invalid")
+                    form.password.classList.add("is-valid")
+                }           
+
+
+                let regEmail = /\S+@\S+\.\S+/;
+                if (!regEmail.test(form.email.value)) {
+                    errors.push("Debe ingresar un email válido");
+                    email.classList.add("is-invalid");
+                    email.classList.remove("is-valid");
+                } else {
+                    email.classList.add("is-valid");
+                    email.classList.remove("is-invalid");
+                    form.password.focus();
+                };
+                
+                if (errors.length > 0) {
+
+                    event.preventDefault();
+                    errorsHtml.innerHTML = "";
+                    errors.forEach(error => {
+                        errorsHtml.innerHTML += "<li>" + error + "</li>"
+                    });
+                } else {
+                    errorsHtml.innerHTML = "";
+                    form.submit();
+                }
+
+                e.preventDefault(); 
+    }) 
+});
+ */
+
