0858b7165cf42b45d8543089dccca09ab6ca515a
Sebastian-Velazquez <sergiovelazquez1693@gmail.com>
2023-04-25 12:26:04 -0300

    asd

--- src/controllers/apiReactController.js
+++ src/controllers/apiReactController.js
@@ -66,6 +66,7 @@ const controlador ={
                         name:element.name,
                         description:element.description,
                         modelo: element.modelo.tipo_de_modelo,//relacion uno a muchos.
+                        precio: element.price,
                         detail: "http://localhost:3001/api/products/" + element.id_product,
                         image:"http://localhost:3001/img/products/" + element.image,
                         count_modelo: element.modelo.length,
